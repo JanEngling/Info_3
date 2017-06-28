0c8505e6d4ce762de455ba55400fc9fadd1fd523
sauttean <andreas.sautter@t-online.de>
2017-06-28 10:11:06 +0200

    Gui
    Änderung was hab noch das kurz rein gemacht was ich mir jetzt gestern noch überleg habe.

--- Aufgabenblatt_1/src/Aufgaben/MainWindow.java
+++ Aufgabenblatt_1/src/Aufgaben/MainWindow.java
@@ -5,27 +5,32 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-
+import javax.swing.JPanel;
 
 
 public class MainWindow extends JFrame {
 	private ControlModel c;
 	
+	private JFrame gui = new JFrame("Control-Developer");
+	private JPanel panel = new JPanel();
+	
 	
 	public void mainWindow(ControlModel c) {
 		this.c = c;
 		
-		// Fenster erzeugen
-		JFrame gui = new JFrame("Control-Developer");
+		// Fenstergröße festlegen 
 		gui.setSize(1920,1080);
-		gui.setLayout(new GridBagLayout());
+		
+		// Aufteilung Fenster 
+		panel.setLayout(new GridBagLayout());
+		
+		// Panel Fenster hinzufügen 
+		gui.add(panel);
 
 		// Menü Leiste hinzufügen
 		MenuBar menu = new MenuBar();
 		gui.setJMenuBar(menu.getJMenuBar(c));
 		
-		ViewMiddle viewMiddle = new ViewMiddle();
-		gui.add(viewMiddle.getPanel());
 
 		// Fenster sichtbar machen
 		gui.setVisible(true);
--- Aufgabenblatt_1/src/Aufgaben/ViewMiddle.java
+++ Aufgabenblatt_1/src/Aufgaben/ViewMiddle.java
@@ -5,12 +5,9 @@ import java.awt.Color;
 import javax.swing.JPanel;
 
 public class ViewMiddle extends JPanel{
-	private JPanel viewMiddle = new JPanel();
 
-	public JPanel getPanel() {
-		viewMiddle.setBackground(Color.red);
-		viewMiddle.setSize(500, 500);
-		return viewMiddle;
+	public void getPanel(ControlModel c) {
+
 	}
 	
 	
