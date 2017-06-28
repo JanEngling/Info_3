05095ee23d458cc54206aed6f4adf6821621aa26
sauttean <andreas.sautter@t-online.de>
2017-06-28 23:41:25 +0200

    An der GUI weiter gemacht
    # Voll am sack
    # Übel der Mist

---
+++ Aufgabenblatt_1/src/Aufgaben/ViewSouth.java
@@ -0,0 +1,19 @@
+package Aufgaben;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class ViewSouth extends JPanel{
+	
+	private ControlModel c;
+	
+	
+	public ViewSouth(ControlModel c) {
+		this.c = c;
+		
+		setBackground(Color.blue);
+	}
+	
+
+}
